8a4350ea418bf6923837a63e5677f6351b2ad233
Ali Skinner <awskinner26@gmail.com>
2015-02-10 10:13:00 +0000

    edit/update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :bio, :twitter, :country, :email, :website, :profile_pic, :remote_profile_pic_url)
